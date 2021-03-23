e1d77daabeb2f6f190385dd9042ea68e20c3503c
Riazzor <flo.sanika+@gmail.com>
2021-03-23 19:17:22 +0100

    Changed from venv + pip to pipenv added db + table in postgres : BDD_Video + video_info.

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
+
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/BDD_Video"
+db = SQLAlchemy(app)
+# db.
+# videos = {}
 
-videos = {}
 
 class Video(Resource):
+    def __init__(self):
+        self.videos_args = reqparse.RequestParser()
+        self.videos_args.add_argument(
+            "title", type=str, help="Name of the video is required", required=True)
+        self.videos_args.add_argument(
+            "views", type=int, help="Name of the video is required", default=0)
+        self.videos_args.add_argument(
+            "likes", type=int, help="Name of the video is required", default=0)
+
+    # CREATE
+    def post(self):
+        pass
+
+    # READ
     def get(self, video_id):
         return videos[video_id]
 
+    # UPDATE
     def put(self, video_id):
+        videos[video_id] = request.form['likes']
+        return videos
+
+    # DELETE
+    def delete(self, video_id):
         pass
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = '127.0.0.1:5000/'
+BASE_URL = 'http://127.0.0.1:5000/'
 
 response = requests.put(BASE_URL + 'video/%d' % 1, {
 	"views": 125,
